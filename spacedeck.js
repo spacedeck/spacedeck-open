9c44aee57ae2615af866159ea5e6d1e9667077d0
Mejans <61360811+Mejans@users.noreply.github.com>
2020-07-22 21:55:32 +0200

    i18n Add Occitan language (#80)
    * Create oc.js
    
    * Add OC to locale list
    
    * Add OC
    
    * Add OC to list
    
    * Update spacedeck.html
    
    * Update root.js

--- spacedeck.js
+++ spacedeck.js
@@ -34,7 +34,7 @@ console.log("Booting Spacedeck Open… (environment: " + app.get('env') + ")");
 app.use(logger(isProduction ? 'combined' : 'dev'));
 
 i18n.expressBind(app, {
-  locales: ["en", "de", "fr"],
+  locales: ["en", "de", "fr", "oc"],
   defaultLocale: "en",
   cookieName: "spacedeck_locale",
   devMode: (app.get('env') == 'development')
