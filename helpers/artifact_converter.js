1b05aaeaf8732cbebe9a9723c6cd6b328da08f61
dm <dm@arillo.net>
2021-01-26 01:38:46 +0100

    add video conversion progress

--- helpers/artifact_converter.js
+++ helpers/artifact_converter.js
@@ -50,12 +50,48 @@ const convertableAudioTypes = [
   "audio/x-hx-aac-adts",
   "audio/aac"];
 
+// ffmpeg progress
+var duration = 0, time = 0, progress = 0;
 
 function getDuration(localFilePath, callback){
   exec.execFile("ffprobe", ["-show_format", "-of", "json", localFilePath], function(error, stdout, stderr) {
     var test = JSON.parse(stdout);
     callback(parseFloat(test.format.duration));
   });
+}
+
+function getConversionProgress(content){
+  // get duration of source
+  var matches = (content) ? content.match(/Duration: (.*?), start:/) : [];
+  if( matches && matches.length>0 ){
+    var rawDuration = matches[1];
+    // convert rawDuration from 00:00:00.00 to seconds.
+    var ar = rawDuration.split(":").reverse();
+    duration = parseFloat(ar[0]);
+    if (ar[1]) duration += parseInt(ar[1]) * 60;
+    if (ar[2]) duration += parseInt(ar[2]) * 60 * 60;
+  } 
+  // get the time 
+  matches = content.match(/time=(.*?) bitrate/g);
+  if( matches && matches.length>0 ){
+    var rawTime = matches.pop();
+    // needed if there is more than one match
+    if (Array.isArray(rawTime)){ 
+        rawTime = rawTime.pop().replace('time=','').replace(' bitrate',''); 
+    } else {
+        rawTime = rawTime.replace('time=','').replace(' bitrate','');
+    }
+
+    // convert rawTime from 00:00:00.00 to seconds.
+    ar = rawTime.split(":").reverse();
+    time = parseFloat(ar[0]);
+    if (ar[1]) time += parseInt(ar[1]) * 60;
+    if (ar[2]) time += parseInt(ar[2]) * 60 * 60;
+
+    //calculate the progress
+    progress = Math.round((time/duration) * 100);
+  }
+  return progress;
 }
 
 function createWaveform(fileName, localFilePath, callback){
@@ -135,7 +171,7 @@ function convertVideo(fileName, filePath, codec, callback, progressCallback) {
   ff.stderr.on('data', function (data) {
     console.log('[ffmpeg-video] stderr: ' + data);
     if (progressCallback) {
-      progressCallback(data);
+      progressCallback(getConversionProgress(""+data));
     }
   });
 
